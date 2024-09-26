f5d458f8c10c8607c6f4c52a398291747bd4aad6
0xCiphera <abikoyet11@gmail.com>
2024-09-26 12:54:52 +0100

    Tasks Completed

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+Welcome to data structures fellas
+"""
+
+
+def print_list_integer(my_list=[]):
+    """
+    this prints all the integers of a list
+    """
+
+    for i in my_list:
+        print("{:d}".format(i))
---
+++ 0x03-python-data_structures/README.md
@@ -0,0 +1,1 @@
+Tasks attempted in python data structure
