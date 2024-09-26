076feaa04a1aed19b55bddcbb8850a3b2fd1c92e
0xCiphera <abikoyet11@gmail.com>
2024-09-26 12:51:36 +0100

    Tasks Completed

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """Prints the sum of a and b"""
+    from add_0 import add
+    a = 1
+    b = 2
+    sum0 = add(a, b)
+    print(f"{a} + {b} = {sum0}")
